5f5844333a2aa65bba08954a150ee67b4f5097e4
gonzalez123l <123398261+gonzalez123l@users.noreply.github.com>
2023-05-11 12:27:14 -0400

    first commit

---
+++ src/graphql/schema.js
@@ -0,0 +1,30 @@
+// Import required info from graphql
+const { GraphQLSchema, GraphQLObjectType } = require('graphql')
+
+// Import queries
+const queries = require('./queries')
+
+// Mutations
+
+const mutations = require('./mutations')
+
+// Define QueryType
+
+const QueryType = new GraphQLObjectType({
+    name: 'QueryType',
+    description: 'This type holds all of my queries',
+    fields: queries
+})
+
+// Define MutationType
+const MutationType = new GraphQLObjectType({
+    name: 'MutationType',
+    description: 'This type holds all of my mutations',
+    fields: mutations
+})
+
+// add mutation to exports
+module.exports = new GraphQLSchema({
+    query: QueryType,
+    mutation: MutationType
+})
---
+++ src/graphql/types.js
@@ -0,0 +1,135 @@
+// Import built in GraphQL data types
+const { User, Question, Quiz, Submission } = require('../models')
+
+// Import our models so that we can interact with the DB
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt, GraphQLFloat, GraphQLList, GraphQLInputObjectType } = require('graphql')
+
+// UserType
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    description: 'User Type',
+    fields: () => ({
+        id: { type: GraphQLID },
+        username: { type: GraphQLString },
+        email: { type: GraphQLString },
+        quizzes: {
+            type: new GraphQLList(QuizType),
+            resolve(parent,args) {
+                return Quiz.find({ userId: parent.id })
+            }
+        },
+        submissions: {
+            type: new GraphQLList(SubmissionType),
+            resolve(parent,args) {
+                return Submission.find({ userId: parent.id })
+            }
+        }
+    })
+})
+
+const QuestionType = new GraphQLObjectType({
+    name: 'Question',
+    description: 'Question Type',
+    fields: () => ({
+        id: { type: GraphQLID },
+        quizId: { type: GraphQLString },
+        title: { type: GraphQLString },
+        correctAnswer: { type: GraphQLString },
+        order: { type: GraphQLInt }
+    })
+})
+
+const QuestionInputType = new GraphQLInputObjectType({
+    name: 'QuestionInput',
+    description: 'Question Input Type',
+    fields: () => ({
+        title: { type: GraphQLString },
+        correctAnswer: { type: GraphQLString },
+        order: { type: GraphQLInt }
+    })
+})
+
+const AnswerInputType = new GraphQLInputObjectType({
+    name: 'AnswerInput',
+    description: 'Answer Input Type',
+    fields: () => ({
+        questionId: { type: GraphQLString },
+        answer: { type: GraphQLString }
+    })
+})
+
+const QuizType = new GraphQLObjectType({
+    name: 'Quiz',
+    description: 'Quiz Type',
+    fields: () => ({
+        id: { type: GraphQLID },
+        slug: { type: GraphQLString },
+        title: { type: GraphQLString },
+        description: { type: GraphQLString },
+        userId: { type: GraphQLString },
+        questions: {
+            type: new GraphQLList(QuestionType),
+            resolve(parent,args) {
+                return Question.find({ quizId: parent.id })
+            }
+        },
+        submissions: {
+            type: new GraphQLList(SubmissionType),
+            resolve(parent,args) {
+                return Submission.find({ quizId: parent.id })
+            }
+        },
+        user: {
+            type: UserType,
+            resolve(parent,args) {
+                return User.findById(parent.userId)
+            }
+        },
+        avgScore: {
+            type: GraphQLFloat,
+            async resolve(parent,args) {
+                const submissions = await Submission.find({ quizId: parent.id })
+
+                let score = 0
+
+                for (const submission of submissions) {
+                    score += submission.score
+                }
+
+                return score / submissions.length
+            }
+        }
+    })
+})
+
+const SubmissionType = new GraphQLObjectType({
+    name: 'Submission',
+    description: 'Submission type',
+    fields: () => ({
+        id: { type: GraphQLID },
+        quizId: { type: GraphQLString },
+        userId: { type: GraphQLString },
+        score: { type: GraphQLInt },
+        user: {
+            type: UserType,
+            resolve(parent, args) {
+                return User.findOne({ _id: parent.userId })
+            }
+        },
+        quiz: {
+            type: QuizType,
+            resolve(parent, args) {
+                return Quiz.findOne({ _id: parent.quizId })
+            }
+        }
+    })
+})
+
+module.exports = {
+    UserType,
+    QuizType,
+    QuestionType,
+    SubmissionType,
+    QuestionInputType,
+    AnswerInputType
+}
